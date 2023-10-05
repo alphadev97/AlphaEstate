315928a848a25091d89d0237d2840e45fec65c0b
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-10-05 18:45:43 +0500

    Header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="bg-blue-100 shadow-md">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+          <span className="text-blue-500">Alpha97</span>
+          <span className="text-blue-700">Estate</span>
+        </h1>
+        <form className="bg-blue-50 p-3 rounded-lg">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="bg-transparent"
+          />
+        </form>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
