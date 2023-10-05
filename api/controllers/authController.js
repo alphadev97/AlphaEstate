8d13f1edc6007e9fc238a6164b85d223cd8c765c
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-10-05 23:55:08 +0500

    authController

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -4,4 +4,5 @@ export const signup = async (req, res) => {
   const { username, email, password } = req.body;
   const newUser = new User({ username, email, password });
   await newUser.save();
+  res.status(201).json({ message: "User is created successfully!" });
 };
