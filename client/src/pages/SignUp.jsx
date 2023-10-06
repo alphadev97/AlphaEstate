bd4049f436e2b9f3b1727be77e98f2621d5cae30
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-10-06 19:00:21 +0500

    SignUp

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,32 @@
 import React from "react";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <div>
+      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+
+      <form className="flex flex-col gap-4">
+        <input
+          type="text"
+          placeholder="Username"
+          className="border p-3 rounded-lg"
+          id="username"
+        />
+        <input
+          type="email"
+          placeholder="Email Address"
+          className="border p-3 rounded-lg"
+          id="email"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="border p-3 rounded-lg"
+          id="password"
+        />
+      </form>
+    </div>
+  );
 };
 
 export default SignUp;
