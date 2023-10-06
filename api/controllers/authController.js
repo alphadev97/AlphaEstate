71efb24e6c252775a3ba3d438f641e45cf6ad412
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-10-06 18:26:21 +0500

    authController

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -5,6 +5,11 @@ export const signup = async (req, res) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashedPassword });
-  await newUser.save();
-  res.status(201).json({ message: "User is created successfully!" });
+
+  try {
+    await newUser.save();
+    res.status(201).json({ message: "User is created successfully!" });
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
 };
