c833b89fcc68a1ec441dec8dc36f449f86992046
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-10-06 19:48:37 +0500

    SignUp

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
   return (
-    <div>
+    <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
 
       <form className="flex flex-col gap-4">
@@ -24,7 +25,17 @@ const SignUp = () => {
           className="border p-3 rounded-lg"
           id="password"
         />
+        <button className="bg-blue-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+          Sign Up
+        </button>
       </form>
+
+      <div className="flex gap-2 mt-5">
+        <p>Have an account?</p>
+        <Link to={"/sign-in"}>
+          <span className="text-blue-700">Sign In</span>
+        </Link>
+      </div>
     </div>
   );
 };
