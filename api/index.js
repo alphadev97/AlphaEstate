8c2cc14968f96038c343be8563f5b1bdf434ad32
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-10-06 18:45:54 +0500

    errorHandler

--- api/index.js
+++ api/index.js
@@ -25,3 +25,16 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+
+// middleware
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error!";
+
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
