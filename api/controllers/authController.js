9bce4ef150fcbd61a794d9b4a530a95dc74c5429
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-10-09 16:40:34 +0500

    SignIn

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -14,3 +14,18 @@ export const signup = async (req, res, next) => {
     next(error);
   }
 };
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+
+    if (!validUser) return next(errorHandler(404, "User not found!"));
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+
+    if (!validPassword) return next(errorHandler(401, "Wrong credentials!"));
+  } catch (error) {
+    next(error);
+  }
+};
