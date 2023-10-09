d8428928745aea69eb6121c3ad0e319e90e8377a
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-10-09 16:59:05 +0500

    SignIn

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -20,19 +20,16 @@ export const signin = async (req, res, next) => {
   const { email, password } = req.body;
   try {
     const validUser = await User.findOne({ email });
-
     if (!validUser) return next(errorHandler(404, "User not found!"));
-
     const validPassword = bcryptjs.compareSync(password, validUser.password);
-
     if (!validPassword) return next(errorHandler(401, "Wrong credentials!"));
-
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc;
 
     res
       .cookie("access_token", token, { httpOnly: true })
       .status(200)
-      .json(validUser);
+      .json(rest);
   } catch (error) {
     next(error);
   }
