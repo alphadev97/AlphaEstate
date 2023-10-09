0b8c2c7b7b939a76e58533deda9f447e944b7a2b
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-10-09 19:53:38 +0500

    GoogleAuth

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import OAuth from "../components/OAuth";
 
 const SignUp = () => {
   const [formData, setFormData] = useState({});
@@ -75,6 +76,7 @@ const SignUp = () => {
         >
           {loading ? "Loading...." : "Sign Up"}
         </button>
+        <OAuth />
       </form>
 
       <div className="flex gap-2 mt-5">
