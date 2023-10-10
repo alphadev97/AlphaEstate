28eb4d77c771db83801dbf8b286e22adbe8dd1b2
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-10-10 17:42:29 +0500

    PrivateRoute

--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import { useSelector } from "react-redux";
+import { Outlet, Navigate } from "react-router-dom";
 
 const PrivateRoute = () => {
-  return <div>PrivateRoute</div>;
+  const { currentUser } = useSelector((state) => state.user);
+
+  return currentUser ? <Outlet /> : <Navigate to={"/sign-in"} />;
 };
 
 export default PrivateRoute;
