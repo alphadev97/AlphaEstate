f4939e6e2a87ba72ca92959c549bed1b9771f6f7
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-10-11 01:22:28 +0500

    clearCookie

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -43,6 +43,7 @@ export const deleteUser = async (req, res, next) => {
 
   try {
     await User.findByIdAndDelete(req.params.id);
+    res.clearCookie("access_token");
     res.status(200).json("User has been deleted!");
   } catch (error) {
     next(error);
