421ac7035c44926ab8e164cbdb8c0e9ac1cc2fb6
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-10-10 21:50:52 +0500

    updateUSer

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,5 +1,38 @@
+import User from "../models/userModel.js";
+import { errorHandler } from "../utils/error.js";
+import bcryptjs from "bcryptjs";
+
 export const test = (req, res) => {
   res.json({
     message: "API route is working!",
   });
+};
+
+export const updateUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(403, "You can only update your own account!"));
+  try {
+    if (req.body.password) {
+      req.body.password = bcryptjs.hashSync(req.body.password, 10);
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          avatar: req.body.avatar,
+        },
+      },
+      { new: true }
+    );
+
+    const { password, ...rest } = updatedUser._doc;
+
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
 };
