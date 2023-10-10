d444edfde29c76d1d64279c12a61aeeac8bbabfa
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-10-11 00:34:19 +0500

    deleteUserRoute

--- api/routes/userRoute.js
+++ api/routes/userRoute.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { test, updateUser } from "../controllers/userController.js";
+import { deleteUser, test, updateUser } from "../controllers/userController.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.get("/test", test);
 router.post("/update/:id", verifyToken, updateUser);
+router.delete("/delete/:id", verifyToken, deleteUser);
 
 export default router;
