4ea2933b68c29f1dc7e6f4af3f6f87564c7af71f
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-10-11 17:09:43 +0500

    signOutRoute

--- api/routes/authRoute.js
+++ api/routes/authRoute.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { google, signin, signup } from "../controllers/authController.js";
+import {
+  google,
+  signin,
+  signout,
+  signup,
+} from "../controllers/authController.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/signin", signin);
 router.post("/google", google);
+router.get("/signout", signout);
 
 export default router;
