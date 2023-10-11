9f4264ec9dcd3a2a6fe1bd59a865bb5fdf9071eb
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-10-11 18:11:38 +0500

    listingController

---
+++ api/controllers/listingController.js
@@ -0,0 +1,11 @@
+import Listing from "../models/listingModel.js";
+
+export const createListing = async (req, res, next) => {
+  try {
+    const listing = await Listing.create(req.body);
+
+    return res.status(201).json(listing);
+  } catch (error) {
+    next(error);
+  }
+};
