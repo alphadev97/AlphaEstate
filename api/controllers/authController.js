c732651e2a96367d076c0544d3e6fa430bb3e39b
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-10-11 17:09:30 +0500

    signOut

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -72,3 +72,12 @@ export const google = async (req, res, next) => {
     next(error);
   }
 };
+
+export const signout = async (req, res, next) => {
+  try {
+    res.clearCookie("access_token");
+    res.status(200).json("User has been logged out!");
+  } catch (error) {
+    next(error);
+  }
+};
