89a16e5cdb8afc283a403fdd170b0a8533ad3e34
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-10-12 23:40:32 +0500

    UserListings

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -240,22 +240,38 @@ const Profile = () => {
         {showListingsError ? "Error showing listings" : ""}
       </p>
 
-      {userListings &&
-        userListings.length > 0 &&
-        userListings.map((listing) => (
-          <div key={listing._id} className="">
-            <Link to={`/listing/${listing._id}`}>
-              <img
-                src={listing.imageUrls[0]}
-                alt="Listing Cover Image"
-                className="h-16 w-16 object-contain"
-              />
-            </Link>
-            <Link to={`/listing/${listing._id}`}>
-              <p className="text-blue-700">{listing.name}</p>
-            </Link>
-          </div>
-        ))}
+      {userListings && userListings.length > 0 && (
+        <div className="flex flex-col gap-4">
+          <h1 className="text-center mt-7 text-2xl font-semibold">
+            Your Listings
+          </h1>
+          {userListings.map((listing) => (
+            <div
+              key={listing._id}
+              className="border rounded-lg p-3 flex justify-between items-center gap-4"
+            >
+              <Link to={`/listing/${listing._id}`}>
+                <img
+                  src={listing.imageUrls[0]}
+                  alt="Listing Cover Image"
+                  className="h-20 w-20 object-contain"
+                />
+              </Link>
+              <Link
+                className=" text-blue-700 font-semibold  hover:underline truncate flex-1"
+                to={`/listing/${listing._id}`}
+              >
+                <p>{listing.name}</p>
+              </Link>
+
+              <div className="flex flex-col items-center">
+                <button className="text-red-700 uppercase">Delete</button>
+                <button className="text-green-700 uppercase">Edit</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
