ac17e300f19928cf8dbc912eb2c50a06118e1d2e
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-10-13 20:49:44 +0500

    Listing

---
+++ client/src/pages/Listing.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+const Listing = () => {
+  const params = useParams();
+  const [listing, setListing] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchListing = async () => {
+      try {
+        setLoading(true);
+        const res = await fetch(`/api/listing/get/${params.listingId}`);
+        const data = await res.json();
+        if (data.success === false) {
+          setError(true);
+          setLoading(false);
+          return;
+        }
+        setListing(data);
+        setLoading(false);
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+      }
+    };
+    fetchListing();
+  }, [params.listingId]);
+
+  return (
+    <main>
+      {loading && <p className="text-center my-7 text-2xl">Loading....</p>}
+    </main>
+  );
+};
+
+export default Listing;
