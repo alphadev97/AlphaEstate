8c534328bd4f57355623dab3a95645ea5e4251dd
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-10-13 20:06:54 +0500

    GetListingRoute

--- api/routes/listingRoute.js
+++ api/routes/listingRoute.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   createListing,
   deleteListing,
+  getListing,
   updateListing,
 } from "../controllers/listingController.js";
 import { verifyToken } from "../utils/verifyUser.js";
@@ -11,5 +12,6 @@ const router = express.Router();
 router.post("/create", verifyToken, createListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
 router.post("/update/:id", verifyToken, updateListing);
+router.post("/get/:id", getListing);
 
 export default router;
