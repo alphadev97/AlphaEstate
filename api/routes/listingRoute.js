bc081348eb7bd7d9cdd1ac44a8623867695d07ee
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-10-13 18:28:36 +0500

    deleteListing

--- api/routes/listingRoute.js
+++ api/routes/listingRoute.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   createListing,
   deleteListing,
+  updateListing,
 } from "../controllers/listingController.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -9,5 +10,6 @@ const router = express.Router();
 
 router.post("/create", verifyToken, createListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
+router.post("/update/:id", verifyToken, updateListing);
 
 export default router;
