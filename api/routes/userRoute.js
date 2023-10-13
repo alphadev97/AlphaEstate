2efc6789f15723be0e12d38a609fd808c8ede51f
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-10-14 00:53:15 +0500

    getUserForComment

--- api/routes/userRoute.js
+++ api/routes/userRoute.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   deleteUser,
+  getUser,
   getUserListing,
   test,
   updateUser,
@@ -13,5 +14,6 @@ router.get("/test", test);
 router.post("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
 router.get("/listings/:id", verifyToken, getUserListing);
+router.get("/:id", verifyToken, getUser);
 
 export default router;
