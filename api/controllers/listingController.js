0d38ec1229e9afb02755377402b1b2b4cfb1b7c6
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-10-13 18:07:06 +0500

    DeleteListingRouteAPI

--- api/controllers/listingController.js
+++ api/controllers/listingController.js
@@ -1,10 +1,30 @@
 import Listing from "../models/listingModel.js";
+import { errorHandler } from "../utils/error.js";
 
 export const createListing = async (req, res, next) => {
   try {
     const listing = await Listing.create(req.body);
 
     return res.status(201).json(listing);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+
+  if (!listing) {
+    return next(errorHandler(404, "Listing not found!"));
+  }
+
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(401, "You can only delete your own listing!"));
+  }
+
+  try {
+    await Listing.findByIdAndDelete(req.params.id);
+    res.status(200).json("Listing has been deleted!");
   } catch (error) {
     next(error);
   }
