4ca3e44c7bba6f0a916bfd811bb7861e8154427a
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-10-13 20:00:21 +0500

    UpdateListing

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -291,7 +291,9 @@ const Profile = () => {
                 >
                   Delete
                 </button>
-                <button className="text-green-700 uppercase">Edit</button>
+                <Link to={`/update-listing/${listing._id}`}>
+                  <button className="text-green-700 uppercase">Edit</button>
+                </Link>
               </div>
             </div>
           ))}
