dcd09b3108715a8349c6726cafdcf493ef733b11
Muhammad Usama <muhammadusama30289@gmail.com>
2023-10-14 18:10:48 +0500

    getUserController

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -64,4 +64,18 @@ export const getUserListing = async (req, res, next) => {
   }
 };
 
-export const getUser = () => {};
+export const getUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (!user) {
+      return next(errorHandler(404, "User not found!"));
+    }
+
+    const { password: pass, ...rest } = user._doc;
+
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
