f3b7084b7bad5770965aebb527756a683a43e7ca
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-10-17 01:30:52 +0500

    listingController

--- api/controllers/listingController.js
+++ api/controllers/listingController.js
@@ -67,7 +67,7 @@ export const getListing = async (req, res, next) => {
 export const getListigs = async (req, res, next) => {
   try {
     const limit = parseInt(req.query.limit) || 9;
-    const startIndex = parseInt(req.body.startIndex) || 0;
+    const startIndex = parseInt(req.query.startIndex) || 0;
     let offer = req.query.offer;
 
     if (offer === undefined || offer === "false") {
