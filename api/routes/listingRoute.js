589d0eb541acacdded964936bc447f4ad44f6399
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-10-16 17:10:32 +0500

    SearchGetListingsRoute

--- api/routes/listingRoute.js
+++ api/routes/listingRoute.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   createListing,
   deleteListing,
+  getListigs,
   getListing,
   updateListing,
 } from "../controllers/listingController.js";
@@ -13,5 +14,6 @@ router.post("/create", verifyToken, createListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
 router.post("/update/:id", verifyToken, updateListing);
 router.get("/get/:id", getListing);
+router.get("/get", getListigs);
 
 export default router;
