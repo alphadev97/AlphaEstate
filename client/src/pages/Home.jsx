84a5b9c55a01440bb372f78769cb5564869d3bdf
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-10-17 17:35:44 +0500

    HomeHero

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      {/* Top */}
+      <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto">
+        <h1 className="text-blue-700 font-bold text-3xl lg:text-6xl">
+          Find your next <span className="text-blue-500">perfect</span>
+          <br /> place with ease
+        </h1>
+        <div className="text-gray-400 text-xs sm:text-sm">
+          Alpha Estate is the best place to find your next perfect place to
+          live.
+          <br />
+          We have a wide range of property to choose from.
+        </div>
+
+        <Link
+          to={"/search"}
+          className="bg-blue-700 text-white p-4 rounded-lg mt-3 hover:opacity-90 w-[160px]"
+        >
+          Let's get started..
+        </Link>
+      </div>
+
+      {/* Swiper */}
+
+      {/* Listing  results for offer, sale and rent*/}
+    </div>
+  );
 };
 
 export default Home;
